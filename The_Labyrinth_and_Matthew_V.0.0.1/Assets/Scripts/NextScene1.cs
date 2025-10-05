ca74952e7acb77b7ba5545671ab24e78fcfa60be
FoxTail <10foxtail10@gmail.com>
2025-10-05 23:41:49 +0300

    Добавление сцен)
    Надоело писать на англ) Добавил 2 сцены и немного анимации. А ещё фонового звука)

---
+++ The_Labyrinth_and_Matthew_V.0.0.1/Assets/Scripts/NextScene1.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class NextScene1 : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    public void OpenGame()
+    {
+        Application.LoadLevel(2);
+    }
+}
